6cae9c1d6bf37d27d2862b7574c73d6b7f7d6260
Tomás Palma <up202108880@edu.fe.up.pt>
2022-10-11 08:58:21 +0100

    Added helper method to create list before tests

---
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -0,0 +1,49 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListAggregatorTest {
+    List<Integer> list;
+    @BeforeEach
+    public void generateList(List<Integer> list) {
+        list = Arrays.asList(1, 2, 4, 2, 5);
+    }
+
+    @Test
+    public void sum() {
+
+        ListAggregator aggregator = new ListAggregator();
+        int sum = aggregator.sum(list);
+
+        Assertions.assertEquals(14, sum);
+    }
+
+    @Test
+    public void max() {
+        ListAggregator aggregator = new ListAggregator();
+        int max = aggregator.max(list);
+
+        Assertions.assertEquals(5, max);
+    }
+
+    @Test
+    public void min() {
+        ListAggregator aggregator = new ListAggregator();
+        int min = aggregator.min(list);
+
+        Assertions.assertEquals(1, min);
+    }
+
+    @Test
+    public void distinct() {
+        ListAggregator aggregator = new ListAggregator();
+        int distinct = aggregator.distinct(list);
+
+        Assertions.assertEquals(4, distinct);
+    }
+}
