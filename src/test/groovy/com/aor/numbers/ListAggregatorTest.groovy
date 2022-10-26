072f0da6fb0541051be4ce3593659853a5ef0462
Tomás Palma <up202108880@edu.fe.up.pt>
2022-10-26 22:07:46 +0100

    added spock and groovy

---
+++ src/test/groovy/com/aor/numbers/ListAggregatorTest.groovy
@@ -0,0 +1,11 @@
+import spock.lang.Specification
+
+class ListAggregatorTest extends Specification {
+    private def list;
+
+    def "Sum of some lists test"() {
+        given:
+            
+    }
+
+}
