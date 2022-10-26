6569f74aaddd70e2aa770a6cdf143f42218ba3db
Tomás Palma <up202108880@edu.fe.up.pt>
2022-10-26 22:18:58 +0100

    completed tests for list deduplicator

--- src/test/groovy/com/aor/numbers/ListDeduplicatorTest.groovy
+++ src/test/groovy/com/aor/numbers/ListDeduplicatorTest.groovy
@@ -1,4 +1,26 @@
 package com.aor.numbers
 
-class ListDeduplicatorTest {
+import spock.lang.Specification
+
+class ListDeduplicatorTest2 extends Specification {
+
+    private def list1, list2;
+
+    def setup() {
+       list1 = Arrays.asList(1, 4, 1, 1, 1);
+        list2 = Arrays.asList(-1, -1, 0, 5, 9, 12, 12);
+    }
+
+    def "Deduplicate of list test"() {
+        given:
+            def listDeduplicator = new ListDeduplicator();
+        when:
+            def sorterStub = Mock(GenericListSorter);
+            sorterStub.sort(list1) >> Arrays.asList(1, 1, 1, 1, 4);
+            sorterStub.sort(list2) >> Arrays.asList(-1, -1, 0, 5, 9, 12, 12);
+        then:
+            listDeduplicator.deduplicate(list1, sorterStub) == Arrays.asList(1, 4);
+            listDeduplicator.deduplicate(list2, sorterStub) == Arrays.asList(-1, 0, 5, 9, 12);
+    }
+
 }
